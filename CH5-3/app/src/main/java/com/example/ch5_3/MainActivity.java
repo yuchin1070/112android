2b3ab74c3145aa84baa0ec5696e6374d49fe664b
yuchin1070 <a111221070@mail.shu.edu.tw>
2024-04-18 17:53:29 +0800

    0418

--- CH5-3/app/src/main/java/com/example/ch5_3/MainActivity.java
+++ CH5-3/app/src/main/java/com/example/ch5_3/MainActivity.java
@@ -2,39 +2,45 @@ package com.example.ch5_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
 
-    private float original_size = 10;
-    //private TextView txvOutput;
+    private float original_size = 20;
+
+    private TextView txvOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView txvOutput = (TextView) findViewById(R.id.txvOutput);
-        txvOutput.setTextSize(original_size);
+        txvOutput = (TextView) findViewById(R.id.txvOutput);
+        txvOutput.setTextSize(TypedValue.COMPLEX_UNIT_PX, original_size);
         Button btnEnlarge = (Button) findViewById(R.id.btnEnlarge);
         btnEnlarge.setOnClickListener(this);
         btnEnlarge.setOnLongClickListener(this);
     }
     @Override
     public void onClick(View v){
-        TextView txvOutput = (TextView) findViewById(R.id.txvOutput);
         float size = txvOutput.getTextSize();
         Log.v("SIZE", "size =" + size );
-        txvOutput.setTextSize(size + 1);
+        txvOutput.setTextSize(px2sp (this,size) +5);
 
     }
     @Override
     public boolean onLongClick(View v){
-        TextView txvOutput = (TextView) findViewById(R.id.txvOutput);
-        txvOutput.setTextSize(original_size);
+        //TextView txvOutput = (TextView) findViewById(R.id.txvOutput);
+        txvOutput.setTextSize(TypedValue.COMPLEX_UNIT_PX,original_size);
         return false;
+    }
+    public static int px2sp(Context context, float pxValue){
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
     }
 }
---
+++ CH5-5/app/src/main/java/com/example/ch5_5/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.ch5_5;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+import color.RED;
+
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener{
+    private TextView txvOutput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txvOutput = (TextView) findViewById(R.id.txvOutput);
+        txvOutput.setTextSize(25);
+        ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.activity_main);
+        layout.setOnTouchListener(this);
+    }
+    @Override
+    public boolean onTouch(View view, MotionEvent event){
+        int act = event.getAction();
+        switch (act){
+            case MotionEvent.ACTION_DOWN:
+                txvOutput.setText("ACTION_DOWN");
+                txvOutput.setTextColor(Color.RED);
+                break;
+            case MotionEvent.ACTION_UP:
+                txvOutput.setText("ACTION_UP");
+                txvOutput.setTextColor(Color.GREEN);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float x = event.getX();
+                float y = event.getY();
+                txvOutput.setText("X =" + x +"\nY = "+y);
+                txvOutput.setTextColor(Color.BLUE);
+                break;
+
+        }
+        return true;
+    }
+}
