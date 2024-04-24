ccfe0b5f55b8b7c607080b5ffb6f819107af91d9
yuchin1070 <a111221070@mail.shu.edu.tw>
2024-04-24 15:45:42 +0800

    0424

--- RadioButton2/app/src/main/java/com/example/radiobutton2/ConfirmationActivity.java
+++ RadioButton2/app/src/main/java/com/example/radiobutton2/ConfirmationActivity.java
@@ -1,10 +1,7 @@
-package com.example.radiobutton1;
+package com.example.radiobutton2;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.RadioButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,20 +13,20 @@ public class ConfirmationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation);
 
-        // 從 Intent 中取得資料
+        // 从 Intent 中获取数据
         Intent intent = getIntent();
         String gender = intent.getStringExtra("gender");
         String ticketType = intent.getStringExtra("ticketType");
         int quantity = intent.getIntExtra("quantity", 0);
-        int totalPrice = intent.getIntExtra("totalPrice", 0);
+        double totalPrice = intent.getDoubleExtra("totalPrice", 0);
 
-        // 找到佈局中的 TextView
+        // 找到布局中的 TextView
         TextView textViewGender = findViewById(R.id.textViewGender);
         TextView textViewTicketType = findViewById(R.id.textViewTicketType);
         TextView textViewQuantity = findViewById(R.id.textViewQuantity);
         TextView textViewTotalPrice = findViewById(R.id.textViewTotalPrice);
 
-        // 將資料設置到 TextView 上
+        // 将数据设置到 TextView 上
         textViewGender.setText("性別: " + gender);
         textViewTicketType.setText("票種: " + ticketType);
         textViewQuantity.setText("購買張數: " + quantity);
