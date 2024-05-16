6ae273f8ba8fbcb003a948ec5214ad6f2f1db96c
yuchin1070 <a111221070@mail.shu.edu.tw>
2024-05-16 17:41:48 +0800

    0516

--- 0516/AlertDialogDemo/app/src/main/java/com/example/alertdialogdemo/MainActivity.java
+++ 0516/AlertDialogDemo/app/src/main/java/com/example/alertdialogdemo/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.alertdialogdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -32,22 +34,50 @@ public class MainActivity extends AppCompatActivity implements DialogInterface.O
                 new AlertDialog.Builder(MainActivity.this)
                         .setTitle("確認")
                         .setMessage("確認結束本程式?")
-                        .setPositiveButton("確定",MainActivity.this)
-                        .setNegativeButton("取消",MainActivity.this)
+                        .setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(MainActivity.this,"按下取消鈕!",Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .show();
+            }
+        });
+        Button BtnColor = (Button) findViewById(R.id.btnColor);
+        BtnColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("選擇一個顏色")
+                        .setItems(new String[]{"紅色", "黃色", "綠色"}, MainActivity.this)
+                        .setNegativeButton("取消", MainActivity.this)
                         .show();
             }
         });
     }
-
     @Override
     public void onClick(DialogInterface dialog, int which) {
-        switch(which){
-            case DialogInterface.BUTTON_POSITIVE:
-                finish();
+        Button btnColor =(Button)findViewById(R.id.btnColor);
+        switch (which){
+            case 0:
+                btnColor.setBackgroundColor(Color.RED);
+                break;
+            case 1:
+                btnColor.setBackgroundColor(Color.YELLOW);
+                break;
+            case 2:
+                btnColor.setBackgroundColor(Color.GREEN);
                 break;
             case DialogInterface.BUTTON_NEGATIVE:
-                Toast.makeText(this,"按下取消鈕!" ,Toast.LENGTH_SHORT).show();
+                Toast.makeText(this,"按下取消鈕!",Toast.LENGTH_SHORT).show();
                 break;
+
         }
     }
 }
