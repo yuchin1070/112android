33cff35dd02294a3c6e9d2b85e287c23bac525f5
yuchin1070 <a111221070@mail.shu.edu.tw>
2024-05-23 17:25:44 +0800

    0523

--- 0523/ExplicitlntentDemo1/app/src/main/java/com/example/explicitlntentdemo1/FActivity.java
+++ 0523/ExplicitlntentDemo1/app/src/main/java/com/example/explicitlntentdemo1/FActivity.java
@@ -2,7 +2,9 @@ package com.example.explicitlntentdemo1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class FActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class FActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_factivity);
+
+        convertTempture();
+    }
+    private void convertTempture(){
+        int c;
+        double f = 0.0;
+        Bundle bundle = this.getIntent().getExtras();
+        if(bundle != null){
+            c = Integer.parseInt(bundle.getString("TEMPC"));
+            f = (9.0* c) /5.0 +32.0;
+            TextView o = (TextView) findViewById(R.id.lblOutput);
+            o.setText("華氏溫度" +Double.toString(f));
+        }
     }
 }
