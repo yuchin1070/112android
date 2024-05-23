63326b8af1df72dbaac955446423ebf1b0032257
yuchin1070 <a111221070@mail.shu.edu.tw>
2024-05-23 16:01:05 +0800

    0523

--- 0516/AlertDialogMultiChoiceDemo/app/src/main/java/com/example/alertdialogmultichoicedemo/MainActivity.java
+++ 0516/AlertDialogMultiChoiceDemo/app/src/main/java/com/example/alertdialogmultichoicedemo/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements DialogInterface.OnClickListener {
+public class MainActivity extends AppCompatActivity implements DialogInterface.OnClickListener,DialogInterface.OnMultiChoiceClickListener{
 
     private String[] items = {"Samsung","OPPO","Apple","ASUS"};
 
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity implements DialogInterface.O
                         .setTitle("請勾選選項?")
                         .setPositiveButton("確定",MainActivity.this)
                         .setNegativeButton("取消",MainActivity.this)
-                        .setMultiChoiceItems(items,itemsChecked,null)
+                        .setMultiChoiceItems(items,itemsChecked,MainActivity.this)
                         .show();
             }
         });
@@ -51,5 +51,12 @@ public class MainActivity extends AppCompatActivity implements DialogInterface.O
                 Toast.makeText(this,"按下取消鈕!" ,Toast.LENGTH_SHORT).show();
                 break;
         }
+    }
+    //new
+    @Override
+    public void onClick(DialogInterface dialog, int which,boolean isChecked) {
+        Toast.makeText(MainActivity.this,
+           items[which]+(isChecked? "勾選":"沒有勾選"),
+                Toast.LENGTH_SHORT).show();
     }
 }
